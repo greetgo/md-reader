6041e960a8fab206f555139e18f7eea87781f94b
ekolpakov <ekolpakov@greet-go.com>
2023-05-11 13:28:57 +0600

    Init empty project after spring boot init

---
+++ src/main/java/kz/greetgo/md_reader/MdReaderApplication.java
@@ -0,0 +1,13 @@
+package kz.greetgo.md_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdReaderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MdReaderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kz/greetgo/md_reader/MdReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.greetgo.md_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
