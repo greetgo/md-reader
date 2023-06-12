471a98b2fc59488ed984e1fc829a78ee3078cd62
ekolpakov <ekolpakov@greet-go.com>
2023-06-12 10:48:37 +0600

    feat(sitemap): Написал красный тест SitemapTest.simple_sizeLimit()

--- src/test/java/kz/greetgo/md_reader/core/sitemap/SitemapTest.java
+++ src/test/java/kz/greetgo/md_reader/core/sitemap/SitemapTest.java
@@ -1,8 +1,10 @@
 package kz.greetgo.md_reader.core.sitemap;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -69,6 +71,59 @@ class SitemapTest extends SitemapTestParent {
     //
 
     assertThat(result).isEqualTo("http://hello.world/status/quo");
+
+  }
+
+  private String extractUrlName(String url) {
+    int i = url.lastIndexOf('/');
+    if (i < 0) {
+      throw new RuntimeException("O19jMbV0kv :: No slashes in " + url);
+    }
+    return url.substring(i + 1);
+  }
+
+  private static String toLen3(int i) {
+    if (i < 10) {
+      return "00" + i;
+    }
+    if (i < 100) {
+      return "0" + i;
+    }
+    return "" + i;
+  }
+
+  @Test
+  void simple_sizeLimit() {
+
+    sitemap.refLimit       = 50_000;
+    sitemap.sizeLimitBytes = 1_000;
+    sitemap.host           = "http://test.kz";
+
+    file("index.md");
+
+    for (int i = 1; i <= 100; i++) {
+      file("010_Top/file_" + toLen3(i) + ".md", "## Файл номер " + i + " - Начинается новый день");
+    }
+
+    String rootXml = sitemap.load(Sitemap.ROOT);
+
+    List<String> list = PathTagParser.parseSitemapIndex(rootXml);
+
+
+    Set<String> refs = list.stream()
+                           .map(this::extractUrlName)
+                           .map(sitemap::load)
+                           .map(PathTagParser::parseUrlSet)
+                           .flatMap(Collection::stream)
+                           .map(this::extractUrlName)
+                           .collect(Collectors.toSet());
+
+    for (int i = 1; i <= 100; i++) {
+      assertThat(refs).contains("file_" + toLen3(i) + ".md");
+    }
+
+    assertThat(list.size()).describedAs("S4RCVOy1mH :: должно быть несколько файлов из-за ограничения размера файла")
+                           .isGreaterThan(3);
 
   }
 }
