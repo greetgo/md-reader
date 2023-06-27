a8bfdf9063b04393420b953a20a29ce9ce8f3c82
ekolpakov <ekolpakov@greet-go.com>
2023-06-27 17:45:11 +0600

    feat(download-pdf): изменения

---
+++ src/main/java/kz/greetgo/md_reader/util/RequestContentType.java
@@ -0,0 +1,14 @@
+package kz.greetgo.md_reader.util;
+
+public enum RequestContentType {
+  PDF(ContentType.Pdf),
+//  DOCX(ContentType.DocX),
+
+  ;
+
+  public final ContentType contentType;
+
+  RequestContentType(ContentType contentType) {
+    this.contentType = contentType;
+  }
+}
